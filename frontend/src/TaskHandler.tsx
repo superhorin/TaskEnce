bfb996f912f36e640848b85b4ae2001d130734ab
masakikaihori <nana7hachi89@gmail.com>
2026-03-02 17:53:37 +0900

    add taskview and handleAdd

--- frontend/src/TaskHandler.tsx
+++ frontend/src/TaskHandler.tsx
@@ -1,14 +1,68 @@
-import type { Task } from "./TaskManeger";
+import { useState } from "react";
+import type { Task } from "./types/task"
 
 type TaskProps = {
 	tasks: Task[];
-	setter: React.Dispatch<React.SetStateAction<Task[]>>;
+	setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 };
 
-export default function TaskHandler ({tasks, setTasks}: TaskProps) {
+type LoadingProps = {
+	loading: boolean;
+	setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+type TaskListCombinedProps = TaskProps & LoadingProps;
+
+export default function TaskHandler ({tasks, setTasks, loading, setLoading}: TaskListCombinedProps) {
+	const [title, setTitle] = useState('');
+
+	const handleSubmit = async(e: React.FormEvent) => {
+		e.preventDefault();
+		if (!title.trim()) return;
+		setLoading(true);
+
+		try {
+			const response = await fetch('http://localhost:3000/tasks', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ title }),
+			});
+			
+			if (!response.ok) {
+				throw new Error('server error: add tasks');
+			}
+
+			const newTask = await response.json();
+			console.log(newTask);
+			setTasks([newTask,...tasks]);
+			
+			setTitle('');
+
+		} catch (error) {
+			console.error('Send error', error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<div>
 			<p>task handler</p>
+			<h3>add new tasks</h3>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+					placeholder="type task name..."
+					disabled={loading}
+				/>
+				<button type="submit" disabled={loading || !title.trim()}>
+					{loading ? 'Sending...' : 'add'}
+				</button>
+			</form>
 		</div>
 	);
 }
--- frontend/src/TaskList.tsx
+++ frontend/src/TaskList.tsx
@@ -1,19 +1,33 @@
-import type { Task } from "./TaskManeger";
+import type { Task } from "./types/task"
 
 type TaskProps = {
 	tasks: Task[];
-	setter: React.Dispatch<React.SetStateAction<Task[]>>;
+	setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 };
 
-export default function TaskList ({tasks, setTasks}: TaskProps) {
+type LoadingProps = {
+	loading: boolean;
+	setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+type TaskListCombinedProps = TaskProps & LoadingProps;
+
+export default function TaskList ({tasks, setTasks, loading, setLoading}: TaskListCombinedProps) {
 	return (
 		<div>
-			<p>list</p>
-			{tasks.length === 0 ? <p>loading...</p> : (
+			<p>Tasks List</p>
+			{!loading && tasks.length === 0 && (
+				<p>No tasks</p>
+			)}
+			{!loading && tasks.length > 0 && (
 				<ul>
-				{tasks.map(task => (
-					<li key={task.id}>{task.title}</li>
-				))}
+					{tasks.map((task) => (
+						<li>
+							<strong>{task.title}</strong>
+							<span>{task.priority}</span>
+							<small>{task.author.name}</small>
+						</li>
+					))}
 				</ul>
 			)}
 		</div>
