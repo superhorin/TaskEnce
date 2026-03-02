dfdd37658a76b665eb774a7b8c0bd14d4e15abe1
masakikaihori <nana7hachi89@gmail.com>
2026-03-02 17:53:37 +0900

    add taskview and handleAdd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
-import TaskManeger from './TaskManeger'
+import TaskManager from './TaskManager'
 
 function App() {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
-    // バックエンド(Nest.js)にリクエストを送る
     fetch('http://localhost:3000')
       .then(res => res.text())
       .then(data => setMessage(data))
@@ -16,8 +15,7 @@ function App() {
     <div>
       <h1>API Test</h1>
       <p>Backend says: {message}</p>
-      {/* <h2>tasks</h2>
-      <TaskManeger /> */}
+      <TaskManager />
     </div>
   )
 }
--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -1,7 +1,23 @@
-export interface CreateTaskRequest {
-	title:			string;
-	description?:	string;
-	priority?:		'LOW' | 'MEDIUM' | 'HIGH'; 
-	assigneeId?:	string;
-	teamId?:		string;
+export type Priority = 'LOW' | 'MEDIUM' | 'HIGH';
+export type Status = 'TODO' | 'IN_PROGRESS' | 'DONE';
+
+export interface User {
+	id:   string;
+	name: string;
+}
+
+export interface Task {
+	id:           string;
+	title:        string;
+	description?: string;
+	priority:     Priority;
+	status:       Status;
+
+	author:      User;
+	assignee?:    User;
+	achiever?:    User;
+
+	createdAt:    string;
+	updatedAt:    string;
+	achievedAt?:  string;
 }
