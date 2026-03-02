34da4f61fcede44f67787282999b339e1d628ca6
masakikaihori <nana7hachi89@gmail.com>
2026-03-02 16:57:31 +0900

    backend/changed task schema and task service

--- frontend/src/TaskManeger.tsx
+++ frontend/src/TaskManeger.tsx
@@ -3,29 +3,30 @@ import TaskHandler from "./TaskHandler";
 import TaskList from "./TaskList";
 
 export interface Task {
-  id:         string;
-  title:      string;
-  createdAt:  string;
-  authoer?: {
-	id:		string;
-	name:	string;
-  }
+	id:						string;
+	title:				string;
+	description?:	string;
+	author?: {
+		id:		string;
+		name:	string;
+	}
+	createdAt:		string;
 }
 
 export default function TaskManeger() {
 	const [tasks, setTasks] = useState<Task[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/tasks')
-      .then(res => res.json())
-      .then(data => setTasks(data))
-      .catch(err => console.error(err))
-  }, [])
+	useEffect(() => {
+		fetch('http://localhost:3000/tasks')
+			.then(res => res.json())
+			.then(data => setTasks(data))
+			.catch(err => console.error(err))
+	}, [])
 
-  return (
-    <div>
-      <TaskHandler tasks = {tasks} setter = {setTasks} />
-      <TaskList tasks = {tasks} setter = {setTasks} />
-    </div>
-  )
+	return (
+		<div>
+			<TaskHandler tasks = {tasks} setter = {setTasks} />
+			<TaskList tasks = {tasks} setter = {setTasks} />
+		</div>
+	)
 }
