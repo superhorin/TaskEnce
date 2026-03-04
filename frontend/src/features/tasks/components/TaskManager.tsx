7196b42fa3f2689f2e62141e4a3fa58fd48e2dfe
masakikaihori <nana7hachi89@gmail.com>
2026-03-04 16:30:27 +0900

    add tailwind and shadcn

--- frontend/src/features/tasks/components/TaskManager.tsx
+++ frontend/src/features/tasks/components/TaskManager.tsx
@@ -3,6 +3,8 @@ import { TaskHandler } from "./TaskHandler";
 import { TaskList } from "./TaskList";
 import { fetchTasks } from "../taskSlice";
 import { useAppDispatch } from "../../../app/hooks";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export const TaskManager = () => {
 	const dispatch = useAppDispatch();
@@ -12,9 +14,9 @@ export const TaskManager = () => {
 	}, [dispatch])
 
 	return (
-		<div>
+		<div className="p-8 bg-slate-50 min-h-screen">
 			<TaskHandler />
 			<TaskList />
 		</div>
-	)
+	);
 }
