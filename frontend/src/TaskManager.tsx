c1bc639d5e32f6099a287ff6098d00274fda98d2
masakikaihori <nana7hachi89@gmail.com>
2026-03-04 21:32:32 +0900

    export host name

--- frontend/src/TaskManager.tsx
+++ frontend/src/TaskManager.tsx
@@ -2,13 +2,14 @@ import {useState, useEffect} from "react";
 import type { Task } from "./types/task";
 import TaskHandler from "./TaskHandler";
 import TaskList from "./TaskList";
+import { API_URL } from './config/env';
 
 export default function TaskManager() {
 	const	[tasks, setTasks] = useState<Task[]>([]);
 	const	[loading, setLoading] = useState<boolean>(true);
 
 	useEffect(() => {
-		fetch('http://localhost:3000/tasks')
+		fetch(`${API_URL}/tasks`)
 			.then(res => res.json())
 			.then(data => setTasks(data))
 			.catch(err => console.error(err))
