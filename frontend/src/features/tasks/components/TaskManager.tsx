a0e859755ec7f08864835c8e6490e42572720139
masakikaihori <nana7hachi89@gmail.com>
2026-03-06 15:19:02 +0900

    design: centered

--- frontend/src/features/tasks/components/TaskManager.tsx
+++ frontend/src/features/tasks/components/TaskManager.tsx
@@ -3,8 +3,6 @@ import { TaskHandler } from "./TaskHandler";
 import { TaskList } from "./TaskList";
 import { fetchTasks } from "../taskSlice";
 import { useAppDispatch } from "../../../app/hooks";
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export const TaskManager = () => {
 	const dispatch = useAppDispatch();
@@ -14,8 +12,10 @@ export const TaskManager = () => {
 	}, [dispatch])
 
 	return (
-		<div className="p-8 bg-slate-50 min-h-screen">
-			<TaskHandler />
+		<div className="max-w-6xl mx-auto">
+			<div className="mb-8">
+				<TaskHandler />
+			</div>
 			<TaskList />
 		</div>
 	);
