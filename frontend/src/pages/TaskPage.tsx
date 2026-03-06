0fcbdf9951e2a60932da138d180d65aee2d868d9
masakikaihori <nana7hachi89@gmail.com>
2026-03-06 15:19:02 +0900

    design: centered

--- frontend/src/pages/TaskPage.tsx
+++ frontend/src/pages/TaskPage.tsx
@@ -1,22 +1,10 @@
 // src/pages/TaskPage.tsx
-import { useEffect, useState } from 'react'
 import { TaskManager } from '../features/tasks/components/TaskManager'
-import { API_URL } from '../config/env'
 
 export const TaskPage = () => {
-	const [message, setMessage] = useState('')
-
-	useEffect(() => {
-		fetch(`${API_URL}`)
-			.then(res => res.text())
-			.then(data => setMessage(data))
-			.catch(err => console.error(err))
-		}, [])
 
 	return (
-		<div>
-			<h1>API Test</h1>
-			<p>Backend says: {message}</p>
+		<div className="p-8 bg-slate-50 min-h-screen">
 			<TaskManager />
 		</div>
 	)
