de102a9134334be573d29c937f3a0686c0a63b59
masakikaihori <nana7hachi89@gmail.com>
2026-03-10 17:30:04 +0900

    feat: swaggger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173', 
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('タスク管理APIの仕様書')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
