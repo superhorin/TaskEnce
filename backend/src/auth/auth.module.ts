4fb34e6328e8cd8d23f6a6a2befc14f0c3d10324
masakikaihori <nana7hachi89@gmail.com>
2026-03-13 19:00:06 +0900

    feat: add login dto and modules

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: 'super-secret-key',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
