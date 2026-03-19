5fac5b42907086e35ab93d20709f792c4a6fed1f
masakikaihori <nana7hachi89@gmail.com>
2026-03-19 18:21:52 +0900

    fix: jwt secret

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,17 @@ import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule, TasksModule, AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
+<<<<<<< HEAD
       expandVariables: true,
+=======
+>>>>>>> 75961c6 (fix: jwt secret)
     }),
   ],
   controllers: [AppController],
