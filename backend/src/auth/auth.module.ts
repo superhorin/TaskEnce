f31f3776a210ab5879e776b7f701befd445758ec
masakikaihori <nana7hachi89@gmail.com>
2026-03-19 18:21:52 +0900

    fix: jwt secret

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,13 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
-    JwtModule.register({
-      secret: 'super-secret-key',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
   providers: [AuthService],
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -2,13 +2,15 @@ import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
 	private readonly logger = new Logger(PrismaService.name);
-	constructor() {
-		const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+	constructor(private configService: ConfigService) {
+		const dbUrl = configService.get<string>('DATABASE_URL');
 
+		const pool = new Pool({ connectionString: dbUrl });
 		const adapter = new PrismaPg(pool);
 
 		super({ adapter });
