f6d6104e95f2341e9e8a8750acce3f0ab938a9fe
masakikaihori <nana7hachi89@gmail.com>
2026-03-24 16:42:22 +0900

    feat: add login logout components #20

--- frontend/src/pages/TaskPage.tsx
+++ frontend/src/pages/TaskPage.tsx
@@ -1,11 +1,15 @@
 // src/pages/TaskPage.tsx
+import { LoginForm } from '@/features/auth/components/LoginForm'
 import { TaskManager } from '../features/tasks/components/TaskManager'
+import { Logout } from '@/features/auth/components/Logout'
 
 export const TaskPage = () => {
 
 	return (
 		<div className="p-8 bg-slate-50 min-h-screen">
 			<TaskManager />
+			<LoginForm />
+			<Logout />
 		</div>
 	)
 }
