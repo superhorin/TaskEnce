42ccd28caf0d903022dda76cd42f2e61e6bd7e7e
masakikaihori <nana7hachi89@gmail.com>
2026-03-24 17:56:52 +0900

    feat: protect route #20

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,26 @@
 import { TaskPage } from '../pages/TaskPage'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ProtectedRoute } from '@/features/auth/ProtectedRoute';
+import { LoginPage } from '@/pages/LoginPage';
 
 function App() {
   return (
-    <div className="app-container">
-      <TaskPage />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route element={<ProtectedRoute />}>
+          {/* <Route element={<MainLayout />}> */}
+            <Route path="/tasks" element={<TaskPage />} />
+            {/* <Route path="/tasks/:id" element={<TaskDetailPage />} />
+            <Route path="/profile" element={<ProfilePage />} /> */}
+          {/* </Route> */}
+        </Route>
+
+        <Route path="*" element={<Navigate replace to="/tasks" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "@/features/auth/components/LoginForm";
+
+export const LoginPage = () => {
+
+	return (
+		<LoginForm />
+	)
+}
