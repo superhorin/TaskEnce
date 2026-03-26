d4db687bc1163e5830b5a196921884a5ded7f166
masakikaihori <nana7hachi89@gmail.com>
2026-03-26 15:57:41 +0900

    originsをenvにまとめた

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,18 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
+  const configService = app.get(ConfigService);
+  const origins = configService.get<string>('ALLOWED_ORIGINS')?.split(',') || ['http://localhost:5173'];
+
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: origins,
   });
 
   await app.listen(3000);
