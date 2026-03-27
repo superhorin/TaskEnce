a13081400c8b4575a831c0c8687b60161c072a14
masakikaihori <nana7hachi89@gmail.com>
2026-03-27 12:36:55 +0900

    feat: マルチテナント化 #31

---
+++ backend/src/auth/get-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import type { User } from "@prisma/client";
+
+export const GetUser = createParamDecorator(
+	(data: unknown, ctx: ExecutionContext): User => {
+		const request = ctx.switchToHttp().getRequest();
+
+		return request.user;
+	},
+);
--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -3,6 +3,8 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-tasks.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import type { User } from '@prisma/client';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('tasks')
@@ -10,20 +12,21 @@ export class TasksController {
 	constructor(private readonly taskService: TasksService) {}
 
 	@Get()
-	getTasks() {
-		return this.taskService.getAllTasks();
+	getTasks(@GetUser() user: User) {
+		return this.taskService.getAllTasks(user);
 	}
 
 	@Post()
-	createTask(@Body() dto: CreateTaskDto) {
-		return this.taskService.createTask(dto);
+	createTask(@Body() dto: CreateTaskDto, @GetUser() user: User) {
+		return this.taskService.createTask(dto, user);
 	}
 
 	@Patch(':id')
 	updateTask(
 		@Param('id') id: string,
-		@Body() dto: UpdateTaskDto
+		@Body() dto: UpdateTaskDto,
+		@GetUser() user: User,
 	) {
-		return this.taskService.updateTask(id, dto);
+		return this.taskService.updateTask(id, dto, user);
 	}
 }
